7498f3d0fc8146212e3f23f92a9d4c6942b5493f
dev.Atef_Turki <56238500+devAtefturki@users.noreply.github.com>
2022-11-26 13:18:19 +0100

    blockchain

---
+++ blockchain/README.md
@@ -0,0 +1,2 @@
+hi to start by typing npm i in each folder with package json
+type "node run Graphcoin.js" inside peer to peer folder
---
+++ blockchain/view/buffer.js
@@ -0,0 +1,4 @@
+const Graphcoin=require("../peer to peer/Graphcoin")
+const data=require("./images.json")
+Graphcoin.
+data.nodes.map((e)=>{})
